5931d5144f1bbe61d8647e1cbace8df1a8da8c2c
Drew Easter <drewheaster@gmail.com>
2023-08-18 01:10:37 -0500

    New loop for copies/extensions

--- file_renamer.py
+++ file_renamer.py
@@ -6,7 +6,6 @@ import requests
 import smartsheet
 import inquirer
 
-
 #tell the user what brand they are working with
 print('--L-COM File Renamer--')
 time.sleep(2)
@@ -18,13 +17,16 @@ fast_photo_path_500='C:/Users/drewh/OneDrive/Desktop/FastPhoto/L-COM/500x500/'+f
 fast_photo_path_250='C:/Users/drewh/OneDrive/Desktop/FastPhoto/L-COM/250x250/'+file
 fast_photo_path_100='C:/Users/drewh/OneDrive/Desktop/FastPhoto/L-COM/100x100/'+file
 
+fast_photo_paths = [ fast_photo_path_500, fast_photo_path_250, fast_photo_path_100 ]
+
+endings = ['_500x500_View1.jpg', '_250x250_View1.jpg', '_100x100_View1.jpg']
+
 images_500='C:/Users/drewh/OneDrive/Desktop/Images/LARGE/'
 images_250='C:/Users/drewh/OneDrive/Desktop/Images/MEDIUM/'
 images_100='C:/Users/drewh/OneDrive/Desktop/Images/SMALL/'
 
-shutil.copy(fast_photo_path_500, fast_photo_path_500.replace('.jpg', '') + '_500x500_View1.jpg')
-shutil.copy(fast_photo_path_250, fast_photo_path_250.replace('.jpg', '') + '_250x250_View1.jpg')
-shutil.copy(fast_photo_path_100, fast_photo_path_100.replace('.jpg', '') + '_100x100_View1.jpg')
+for i in range(len(fast_photo_paths)):
+    shutil.copy(fast_photo_paths[i], fast_photo_paths[i].replace('.jpg', '') + endings[i])
 
 my_paths = {
     glob.glob(os.path.join(fast_photo_path_500.replace(file,''), '*_500x500_View1*'), recursive=True)[0]: images_500,
