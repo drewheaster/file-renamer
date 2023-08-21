a5248c9a0b7d55d2a15772ad282ab8c7424e7730
Drew Easter <drewheaster@gmail.com>
2023-08-21 03:02:17 -0500

    New version of file-renamer.py added

--- file_renamer.py
+++ file_renamer.py
@@ -4,7 +4,6 @@ import shutil
 import glob
 import requests
 import smartsheet
-import inquirer
 import sys
 
 print('--L-COM File Renamer--')
@@ -20,7 +19,6 @@ while True:
         break
     contents.append(line)
 
-print(contents[0])
 
 fast_photo_path_500='C:/Users/drewh/OneDrive/Desktop/FastPhoto/L-COM/500x500/'+ file
 fast_photo_path_250='C:/Users/drewh/OneDrive/Desktop/FastPhoto/L-COM/250x250/'+ file
@@ -34,24 +32,36 @@ images_500='C:/Users/drewh/OneDrive/Desktop/Images/LARGE/'
 images_250='C:/Users/drewh/OneDrive/Desktop/Images/MEDIUM/'
 images_100='C:/Users/drewh/OneDrive/Desktop/Images/SMALL/'
 
+new_contents=[]
+new_contents_500=[]
+new_contents_250=[]
+new_contents_100=[]
+
+for x in range(len(fast_photo_paths)):
+    for i in range(len(contents)):
+        new_contents.append(shutil.copy2(fast_photo_paths[x], fast_photo_paths[x].replace(file, contents[i] + '.jpg')))
+
 for i in range(len(contents)):
-    for x in range(len(fast_photo_paths)):
-        shutil.copy2(fast_photo_paths[x], fast_photo_paths[x].replace(file, contents[i] + '.jpg'))
+    new_contents_500.append(fast_photo_path_500.replace(file,contents[i]+'.jpg'))
+    new_contents_250.append(fast_photo_path_250.replace(file,contents[i]+'.jpg'))
+    new_contents_100.append(fast_photo_path_100.replace(file,contents[i]+'.jpg'))
+
 
 for i in range(len(fast_photo_paths)):
-    shutil.copy(fast_photo_paths[i], fast_photo_paths[i].replace('.jpg', '') + endings[i])
+     shutil.copy(fast_photo_paths[i], fast_photo_paths[i].replace('.jpg', '') + endings[i])
 
-#write a function to take the elements of list [contents] and create directory paths to them
+for i in range(len(new_contents_500)):
+    shutil.copy2(new_contents_500[i], new_contents_500[i].replace('.jpg', '_500x500_View1.jpg'))
+    shutil.copy2(new_contents_250[i], new_contents_250[i].replace('.jpg', '_250x250_View1.jpg'))
+    shutil.copy2(new_contents_100[i], new_contents_100[i].replace('.jpg', '_100x100_View1.jpg'))
+    
 
-my_paths = {
-    glob.glob(os.path.join(fast_photo_path_500.replace(file,''), '*_500x500_View1*'), recursive=True)[0]: images_500,
-    glob.glob(os.path.join(fast_photo_path_250.replace(file,''), '*_250x250_View1*'), recursive=True)[0]: images_250, 
-    glob.glob(os.path.join(fast_photo_path_100.replace(file,''), '*_100x100_View1*'), recursive=True)[0]: images_100
-}
+extensions_500=(glob.glob(os.path.join(fast_photo_path_500.replace(file,''), '*_500x500_View1*'), recursive=True))
+extensions_250=(glob.glob(os.path.join(fast_photo_path_250.replace(file,''), '*_250x250_View1*'), recursive=True))
+extensions_100=(glob.glob(os.path.join(fast_photo_path_100.replace(file,''), '*_100x100_View1*'), recursive=True))
 
-def file_mover(my_paths):
-    for i in my_paths:
-        dst_path = os.path.join(my_paths[i], os.path.basename(i))
-        shutil.move(i, dst_path)
 
-file_mover(my_paths)
+for i in range(len(extensions_500)):
+    shutil.move(extensions_500[i], images_500)
+    shutil.move(extensions_250[i], images_250)
+    shutil.move(extensions_100[i], images_100)
